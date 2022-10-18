467c0e1ab04aeef3ea7b91e42502200b27f1b4d6
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2022-10-17 21:39:36 -0300

    ref: remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,27 @@
-import { Flex, Button, Stack } from '@chakra-ui/react';
-import { Input } from '../components/Form/Input';
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 export default function SignIn() {
   return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-    >
-      <Flex 
-      as="form" 
-      w="100%" 
-      maxWidth={360} 
-      bg="gray.800"
-      p="8"
-      borderRadius={8}
-      flexDir="column"
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
       >
         <Stack spacing="4">
           <Input type="email" name="email" label="E-mail"></Input>
           <Input type="password" name="password" label="Senha"></Input>
-          </Stack>
-          
-         <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
